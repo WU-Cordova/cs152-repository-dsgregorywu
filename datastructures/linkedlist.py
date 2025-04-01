b6acb755b3f4053d57540d3d2d6a2c948be6900d
dsgregorywu <dsgregory@willamette.edu>
2025-03-31 17:33:07 -0700

    new

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
-
 from dataclasses import dataclass
 import os
 from typing import Optional, Sequence
 from datastructures.ilinkedlist import ILinkedList, T
-
 
 class LinkedList[T](ILinkedList[T]):
 
@@ -21,15 +19,17 @@ class LinkedList[T](ILinkedList[T]):
         self.data_type = data_type
 
     @staticmethod
-    def from_sequence(sequence: Sequence[T], data_type: type=object) -> LinkedList[T]:
+    def from_sequence(sequence: Sequence[T], data_type: type = object) -> LinkedList[T]:
         linked_list = LinkedList(data_type=data_type)
         for item in sequence:
             linked_list.append(item)
         return linked_list
 
     def append(self, item: T) -> None:
+        if not isinstance(item, self.data_type):
+            raise TypeError(f"Item must be of type {self.data_type}")
         new_node = LinkedList.Node(data=item)
-        if self.tail is None:  
+        if self.tail is None:
             self.head = new_node
             self.tail = new_node
         else:
@@ -39,14 +39,20 @@ class LinkedList[T](ILinkedList[T]):
         self.count += 1
 
     def prepend(self, item: T) -> None:
+        if not isinstance(item, self.data_type):
+            raise TypeError(f"Item must be of type {self.data_type}")
         new_node = LinkedList.Node(data=item)
         new_node.next = self.head
         if self.head:
             self.head.previous = new_node
         self.head = new_node
+        if self.tail is None:
+            self.tail = new_node
         self.count += 1
 
     def insert_before(self, target: T, item: T) -> None:
+        if not isinstance(item, self.data_type):
+            raise TypeError(f"Item must be of type {self.data_type}")
         if self.head is None:
             raise ValueError("List is empty")
         current = self.head
@@ -68,30 +74,33 @@ class LinkedList[T](ILinkedList[T]):
         self.count += 1
 
     def insert_after(self, target: T, item: T) -> None:
-        if self.head is None: raise ValueError("List is empty")
+        if not isinstance(item, self.data_type):
+            raise TypeError(f"Item must be of type {self.data_type}")
+        if self.head is None:
+            raise ValueError("List is empty")
         current = self.head
         while current and current.data != target:
             current = current.next
-        if current is None:raise ValueError(f"Target {target} not found in the list")
+        if current is None:
+            raise ValueError(f"Target {target} not found in the list")
         new_node = LinkedList.Node(data=item)
-        if current.previous is None:
-            new_node.next = self.head
-            self.head.previous = new_node
-            self.head = new_node
+        new_node.previous = current
+        new_node.next = current.next
+        if current.next is not None:
+            current.next.previous = new_node
         else:
-            previous_node = current.previous
-            previous_node.next = new_node
-            new_node.previous = previous_node
-            new_node.next = current
-            current.previous = new_node
+            self.tail = new_node
+        current.next = new_node
         self.count += 1
 
     def remove(self, item: T) -> None:
-        if self.head is None: raise ValueError("List is empty")
+        if self.head is None:
+            raise ValueError("List is empty")
         current = self.head
         while current and current.data != item:
             current = current.next
-        if current is None: raise ValueError(f"{item} not found in the list")
+        if current is None:
+            raise ValueError(f"{item} not found in the list")
         if current.previous is None:
             self.head = current.next
             if self.head:
@@ -107,77 +116,105 @@ class LinkedList[T](ILinkedList[T]):
         self.count -= 1
 
     def remove_all(self, item: T) -> None:
-
-        raise NotImplementedError("LinkedList.remove_all is not implemented")
+        if self.head is None:
+            raise ValueError("List is empty")
+        current = self.head
+        while current:
+            next_node = current.next
+            if current.data == item:
+                if current.previous is None:
+                    self.head = current.next
+                    if self.head:
+                        self.head.previous = None
+                else:
+                    current.previous.next = current.next
+                if current.next is None:
+                    self.tail = current.previous
+                    if self.tail:
+                        self.tail.next = None
+                else:
+                    current.next.previous = current.previous
+                self.count -= 1
+            current = next_node
 
     def pop(self) -> T:
-        if self.tail is None: raise IndexError("Pop from empty list")
+        if self.tail is None:
+            raise IndexError("Pop from empty list")
         data = self.tail.data
         self.tail = self.tail.previous
         if self.tail is not None:
             self.tail.next = None
-        else: self.head = None
+        else:
+            self.head = None
         self.count -= 1
         return data
 
     def pop_front(self) -> T:
-        if self.tail is None: raise IndexError("Pop from empty list")
+        if self.head is None:
+            raise IndexError("Pop from empty list")
         data = self.head.data
         self.head = self.head.next
         if self.head is not None:
             self.head.previous = None
-        else: self.tail = None
+        else:
+            self.tail = None
         self.count -= 1
         return data
 
     @property
     def front(self) -> T:
-        return self.head
+        if self.head is None:
+            raise IndexError("List is empty")
+        return self.head.data
 
     @property
     def back(self) -> T:
-        return self.tail
+        if self.tail is None:
+            raise IndexError("List is empty")
+        return self.tail.data
 
     @property
     def empty(self) -> bool:
-        if self.head: return True
-        else: return False
+        return self.head is None
 
     def __len__(self) -> int:
         return self.count
-    
+
     def clear(self) -> None:
-        raise NotImplementedError("LinkedList.clear is not implemented")
+        self.head = None
+        self.tail = None
+        self.count = 0
 
     def __contains__(self, item: T) -> bool:
-        if self.head is None: raise ValueError("List is empty")
         current = self.head
-        while current and current.data != item:
+        while current:
+            if current.data == item:
+                return True
             current = current.next
-        if current is None: bool = False
-        else: bool = True
-        return bool
+        return False
 
     def __iter__(self) -> LinkedList[T]:
-        self._current = self.head  
+        self._current = self.head
         return self
 
     def __next__(self) -> T:
         if self._current is None:
-            raise StopIteration  
+            raise StopIteration
         data = self._current.data
-        self._current = self._current.next 
+        self._current = self._current.next
         return data
-    
+
     def __reversed__(self):
-        current = self.tail 
+        current = self.tail
         while current:
             yield current.data
             current = current.previous
-        
+
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, LinkedList): return False
-        if len(self) != len(other): return False
+        if not isinstance(other, LinkedList):
+            return False
+        if len(self) != len(other):
+            return False
         current_self = self.head
         current_other = other.head
         while current_self and current_other:
